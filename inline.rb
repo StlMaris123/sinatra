5ef5562cf1a1edb6b1ed7781b6fdd883ae9f53ee
StlMaris123 <stellamarisnjage@gmail.com>
2016-10-12 19:07:42 +0300

    handle 404 errors

---
+++ inline.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+get '/index' do
+  erb :index
+end
+
+_END_
+
+@@index
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Inline template>
+</head>
+
+<body>
+<h1> Ni kujaribu tu</h1>
+</body>
+</html>
