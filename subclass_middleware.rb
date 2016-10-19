b84fbcb19fccedecac62146318e996b0c184fcc6
StlMaris123 <stellamarisnjage@gmail.com>
2016-10-19 18:44:25 +0300

    automatically picking up subclasses as middleware

---
+++ subclass_middleware.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+
+class ApplicationController < Sinatra::Base
+  def self.inherited(subclass)
+    super
+    use subclass
+  end
+
+  enable :loggig
+end
+
+class ExampleController < Sinatra::Base
+  get('example') { "Example!" }
+end
+
+#works with dynamically generated applications too
+Sinatra.new ApplicationController do
+  get '/' do
+    "see the <a href='/example'>example</a"
+  end
+end
+
+ApplicationController.run!
+
