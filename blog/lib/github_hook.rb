f5502e1b2557cbecf41fa5404b3eb7fbc7b88208
StlMaris123 <stellamarisnjage@gmail.com>
2016-10-20 14:54:00 +0300

    regenerating content

---
+++ blog/lib/github_hook.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require 'time'
+
+class GithubHook < Sinatra::Base
+  post '/update' do
+    app.settings.reset!
+    load app.settings.app_file
+    content_type :txt
+    "ok"
+  end
+end
